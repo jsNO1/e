25f13439c84aa664ba49e82ca880f98666fb4585
jsNo1 <79303989+jsNO1@users.noreply.github.com>
2021-06-22 17:39:47 +0800

    Update jd_dreamFactory.js

--- lxk/jd_dreamFactory.js
+++ lxk/jd_dreamFactory.js
@@ -11,7 +11,7 @@
 ============Quantumultx===============
 [task_local]
 #京喜工厂
-10 * * * * jd_dreamFactory.js, tag=京喜工厂, img-url=https://github.com/58xinian/icon/raw/master/jdgc.png, enabled=true
+0,30 6,7,8,9,12,18,21 * * * jd_dreamFactory.js, tag=京喜工厂, img-url=https://github.com/58xinian/icon/raw/master/jdgc.png, enabled=true
 
 ================Loon==============
 [Script]
