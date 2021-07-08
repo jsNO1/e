946e294c465d5ae91a27fd374738acded270c186
jsNo1 <79303989+jsNO1@users.noreply.github.com>
2021-07-08 15:55:46 +0800

    Update jd_summer_movement_help.js

--- em/jd_summer_movement_help.js
+++ em/jd_summer_movement_help.js
@@ -13,7 +13,6 @@ cron "0 0 * * *" script-path=jd_summer_movement_help.js,tag=燃动夏季助力
 
 ============小火箭=========
 燃动夏季助力 = type=cron,script-path=jd_summer_movement_help.js, cronexpr="0 0 * * *", timeout=3600, enable=true
-
 脚本原址
 https://wbbny.m.jd.com/babelDiy/Zeus/2rtpffK8wqNyPBH6wyUDuBKoAbCt/index.html
 
@@ -78,7 +77,7 @@ const UA = $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT :
       $.hotFlag = false; //是否火爆
       console.log(`\n*****开始【京东账号${$.index}】${$.nickName || $.UserName}*****\n`);
       console.log(`\n如有未完成的任务，请多执行几次\n`);
-      //await movement()
+      await movement()
       if($.hotFlag)$.secretpInfo[$.UserName] = false;//火爆账号不执行助力
     }
   }
@@ -177,133 +176,133 @@ async function movement() {
     }
     
     await takePostRequest('olympicgames_getTaskDetail');
-    await $.wait(1000);
-    //做任务
-    for (let i = 0; i < $.taskList.length && !$.hotFlag; i++) {
-      $.oneTask = $.taskList[i];
-      if ([1, 3, 5, 7, 9, 26].includes($.oneTask.taskType) && $.oneTask.status === 1) {
-        $.activityInfoList = $.oneTask.shoppingActivityVos || $.oneTask.brandMemberVos || $.oneTask.followShopVo || $.oneTask.browseShopVo;
-        for (let j = 0; j < $.activityInfoList.length; j++) {
-          $.oneActivityInfo = $.activityInfoList[j];
-          if ($.oneActivityInfo.status !== 1 || !$.oneActivityInfo.taskToken) {
-            continue;
-          }
-          $.callbackInfo = {};
-          console.log(`做任务：${$.oneActivityInfo.title || $.oneActivityInfo.taskName || $.oneActivityInfo.shopName};等待完成`);
-          await takePostRequest('olympicgames_doTaskDetail');
-          if ($.callbackInfo.code === 0 && $.callbackInfo.data && $.callbackInfo.data.result && $.callbackInfo.data.result.taskToken) {
-            await $.wait(getRndInteger(7000, 8000));
-            let sendInfo = encodeURIComponent(`{"dataSource":"newshortAward","method":"getTaskAward","reqParams":"{\\"taskToken\\":\\"${$.callbackInfo.data.result.taskToken}\\"}","sdkVersion":"1.0.0","clientLanguage":"zh"}`)
-            await callbackResult(sendInfo)
-          } else if ($.oneTask.taskType === 5 || $.oneTask.taskType === 3 || $.oneTask.taskType === 26) {
-            await $.wait(2000);
-            console.log(`任务完成`);
-          } else {
-            console.log($.callbackInfo);
-            console.log(`任务失败`);
-            await $.wait(3000);
+    // await $.wait(1000);
+    // //做任务
+    // for (let i = 0; i < $.taskList.length && !$.hotFlag; i++) {
+    //   $.oneTask = $.taskList[i];
+    //   if ([1, 3, 5, 7, 9, 26].includes($.oneTask.taskType) && $.oneTask.status === 1) {
+    //     $.activityInfoList = $.oneTask.shoppingActivityVos || $.oneTask.brandMemberVos || $.oneTask.followShopVo || $.oneTask.browseShopVo;
+    //     for (let j = 0; j < $.activityInfoList.length; j++) {
+    //       $.oneActivityInfo = $.activityInfoList[j];
+    //       if ($.oneActivityInfo.status !== 1 || !$.oneActivityInfo.taskToken) {
+    //         continue;
+    //       }
+    //       $.callbackInfo = {};
+    //       console.log(`做任务：${$.oneActivityInfo.title || $.oneActivityInfo.taskName || $.oneActivityInfo.shopName};等待完成`);
+    //       await takePostRequest('olympicgames_doTaskDetail');
+    //       if ($.callbackInfo.code === 0 && $.callbackInfo.data && $.callbackInfo.data.result && $.callbackInfo.data.result.taskToken) {
+    //         await $.wait(getRndInteger(7000, 8000));
+    //         let sendInfo = encodeURIComponent(`{"dataSource":"newshortAward","method":"getTaskAward","reqParams":"{\\"taskToken\\":\\"${$.callbackInfo.data.result.taskToken}\\"}","sdkVersion":"1.0.0","clientLanguage":"zh"}`)
+    //         await callbackResult(sendInfo)
+    //       } else if ($.oneTask.taskType === 5 || $.oneTask.taskType === 3 || $.oneTask.taskType === 26) {
+    //         await $.wait(2000);
+    //         console.log(`任务完成`);
+    //       } else {
+    //         console.log($.callbackInfo);
+    //         console.log(`任务失败`);
+    //         await $.wait(3000);
 
-          }
+    //       }
 
-        }
-        break
-      } else if ($.oneTask.taskType === 2 && $.oneTask.status === 1 && $.oneTask.scoreRuleVos[0].scoreRuleType === 2){
-        console.log(`做任务：${$.oneTask.taskName};等待完成 (实际不会添加到购物车)`);
-        $.taskId = $.oneTask.taskId;
-        $.feedDetailInfo = {};
-        await takePostRequest('olympicgames_getFeedDetail');
-        let productList = $.feedDetailInfo.productInfoVos;
-        let needTime = Number($.feedDetailInfo.maxTimes) - Number($.feedDetailInfo.times);
-        for (let j = 0; j < productList.length && needTime > 0; j++) {
-          if(productList[j].status !== 1){
-            continue;
-          }
-          $.taskToken = productList[j].taskToken;
-          console.log(`加购：${productList[j].skuName}`);
-          await takePostRequest('add_car');
-          await $.wait(1500);
-          needTime --;
-        }
-      }else if ($.oneTask.taskType === 2 && $.oneTask.status === 1 && $.oneTask.scoreRuleVos[0].scoreRuleType === 0){
-        $.activityInfoList = $.oneTask.productInfoVos ;
-        for (let j = 0; j < $.activityInfoList.length; j++) {
-          $.oneActivityInfo = $.activityInfoList[j];
-          if ($.oneActivityInfo.status !== 1 || !$.oneActivityInfo.taskToken) {
-            continue;
-          }
-          $.callbackInfo = {};
-          console.log(`做任务：浏览${$.oneActivityInfo.skuName};等待完成`);
-          await takePostRequest('olympicgames_doTaskDetail');
-          if ($.oneTask.taskType === 2) {
-            await $.wait(2000);
-            console.log(`任务完成`);
-          } else {
-            console.log($.callbackInfo);
-            console.log(`任务失败`);
-            await $.wait(3000);
-          }
-        }
-      }
-    }
+    //     }
+    //     break
+    //   } else if ($.oneTask.taskType === 2 && $.oneTask.status === 1 && $.oneTask.scoreRuleVos[0].scoreRuleType === 2){
+    //     console.log(`做任务：${$.oneTask.taskName};等待完成 (实际不会添加到购物车)`);
+    //     $.taskId = $.oneTask.taskId;
+    //     $.feedDetailInfo = {};
+    //     await takePostRequest('olympicgames_getFeedDetail');
+    //     let productList = $.feedDetailInfo.productInfoVos;
+    //     let needTime = Number($.feedDetailInfo.maxTimes) - Number($.feedDetailInfo.times);
+    //     for (let j = 0; j < productList.length && needTime > 0; j++) {
+    //       if(productList[j].status !== 1){
+    //         continue;
+    //       }
+    //       $.taskToken = productList[j].taskToken;
+    //       console.log(`加购：${productList[j].skuName}`);
+    //       await takePostRequest('add_car');
+    //       await $.wait(1500);
+    //       needTime --;
+    //     }
+    //   }else if ($.oneTask.taskType === 2 && $.oneTask.status === 1 && $.oneTask.scoreRuleVos[0].scoreRuleType === 0){
+    //     $.activityInfoList = $.oneTask.productInfoVos ;
+    //     for (let j = 0; j < $.activityInfoList.length; j++) {
+    //       $.oneActivityInfo = $.activityInfoList[j];
+    //       if ($.oneActivityInfo.status !== 1 || !$.oneActivityInfo.taskToken) {
+    //         continue;
+    //       }
+    //       $.callbackInfo = {};
+    //       console.log(`做任务：浏览${$.oneActivityInfo.skuName};等待完成`);
+    //       await takePostRequest('olympicgames_doTaskDetail');
+    //       if ($.oneTask.taskType === 2) {
+    //         await $.wait(2000);
+    //         console.log(`任务完成`);
+    //       } else {
+    //         console.log($.callbackInfo);
+    //         console.log(`任务失败`);
+    //         await $.wait(3000);
+    //       }
+    //     }
+    //   }
+    // }
     // 店铺
-    if (new Date().getUTCHours() + 8 >= 14 && new Date().getUTCHours() + 8 <= 19){//为了避免代码执行太久，下午2点到7点才做店铺任务
-      console.log(`去做店铺任务`);
-      $.shopInfoList = [];
-      await takePostRequest('qryCompositeMaterials');
-      for (let i = 0; i < $.shopInfoList.length; i++) {
-        $.shopSign = $.shopInfoList[i].extension.shopId;
-        console.log(`执行第${i+1}个店铺任务：${$.shopInfoList[i].name} ID:${$.shopSign}`);
-        $.shopResult = {};
-        await takePostRequest('olympicgames_shopLotteryInfo');
-        await $.wait(1000);
-        if(JSON.stringify($.shopResult) === `{}`) continue;
-        $.shopTask = $.shopResult.taskVos || [];
-        for (let i = 0; i < $.shopTask.length; i++) {
-          $.oneTask = $.shopTask[i];
-          if($.oneTask.taskType === 21 || $.oneTask.taskType === 14 || $.oneTask.status !== 1){continue;} //不做入会//不做邀请
-          $.activityInfoList = $.oneTask.brandMemberVos || $.oneTask.followShopVo || $.oneTask.shoppingActivityVos || $.oneTask.browseShopVo || $.oneTask.simpleRecordInfoVo;
-          if($.oneTask.taskType === 12){//签到
-            $.oneActivityInfo =  $.activityInfoList;
-            console.log(`店铺签到`);
-            await takePostRequest('olympicgames_bdDoTask');
-            continue;
-          }
-          for (let j = 0; j < $.activityInfoList.length; j++) {
-            $.oneActivityInfo = $.activityInfoList[j];
-            if ($.oneActivityInfo.status !== 1 || !$.oneActivityInfo.taskToken) {
-              continue;
-            }
-            $.callbackInfo = {};
-            console.log(`做任务：${$.oneActivityInfo.subtitle || $.oneActivityInfo.title || $.oneActivityInfo.taskName || $.oneActivityInfo.shopName};等待完成`);
-            await takePostRequest('olympicgames_doTaskDetail');
-            if ($.callbackInfo.code === 0 && $.callbackInfo.data && $.callbackInfo.data.result && $.callbackInfo.data.result.taskToken) {
-              await $.wait(8000);
-              let sendInfo = encodeURIComponent(`{"dataSource":"newshortAward","method":"getTaskAward","reqParams":"{\\"taskToken\\":\\"${$.callbackInfo.data.result.taskToken}\\"}","sdkVersion":"1.0.0","clientLanguage":"zh"}`)
-              await callbackResult(sendInfo)
-            } else  {
-              await $.wait(2000);
-              console.log(`任务完成`);
-            }
-          }
-        }
-        await $.wait(1000);
-        let boxLotteryNum = $.shopResult.boxLotteryNum;
-        for (let j = 0; j < boxLotteryNum; j++) {
-          console.log(`开始第${j+1}次拆盒`)
-          //抽奖
-          await takePostRequest('olympicgames_boxShopLottery');
-          await $.wait(3000);
-        }
-        // let wishLotteryNum = $.shopResult.wishLotteryNum;
-        // for (let j = 0; j < wishLotteryNum; j++) {
-        //   console.log(`开始第${j+1}次能量抽奖`)
-        //   //抽奖
-        //   await takePostRequest('zoo_wishShopLottery');
-        //   await $.wait(3000);
-        // }
-        await $.wait(3000);
-      }
-    }
+    // if (new Date().getUTCHours() + 8 >= 14 && new Date().getUTCHours() + 8 <= 19){//为了避免代码执行太久，下午2点到7点才做店铺任务
+    //   console.log(`去做店铺任务`);
+    //   $.shopInfoList = [];
+    //   await takePostRequest('qryCompositeMaterials');
+    //   for (let i = 0; i < $.shopInfoList.length; i++) {
+    //     $.shopSign = $.shopInfoList[i].extension.shopId;
+    //     console.log(`执行第${i+1}个店铺任务：${$.shopInfoList[i].name} ID:${$.shopSign}`);
+    //     $.shopResult = {};
+    //     await takePostRequest('olympicgames_shopLotteryInfo');
+    //     await $.wait(1000);
+    //     if(JSON.stringify($.shopResult) === `{}`) continue;
+    //     $.shopTask = $.shopResult.taskVos || [];
+    //     for (let i = 0; i < $.shopTask.length; i++) {
+    //       $.oneTask = $.shopTask[i];
+    //       if($.oneTask.taskType === 21 || $.oneTask.taskType === 14 || $.oneTask.status !== 1){continue;} //不做入会//不做邀请
+    //       $.activityInfoList = $.oneTask.brandMemberVos || $.oneTask.followShopVo || $.oneTask.shoppingActivityVos || $.oneTask.browseShopVo || $.oneTask.simpleRecordInfoVo;
+    //       if($.oneTask.taskType === 12){//签到
+    //         $.oneActivityInfo =  $.activityInfoList;
+    //         console.log(`店铺签到`);
+    //         await takePostRequest('olympicgames_bdDoTask');
+    //         continue;
+    //       }
+    //       for (let j = 0; j < $.activityInfoList.length; j++) {
+    //         $.oneActivityInfo = $.activityInfoList[j];
+    //         if ($.oneActivityInfo.status !== 1 || !$.oneActivityInfo.taskToken) {
+    //           continue;
+    //         }
+    //         $.callbackInfo = {};
+    //         console.log(`做任务：${$.oneActivityInfo.subtitle || $.oneActivityInfo.title || $.oneActivityInfo.taskName || $.oneActivityInfo.shopName};等待完成`);
+    //         await takePostRequest('olympicgames_doTaskDetail');
+    //         if ($.callbackInfo.code === 0 && $.callbackInfo.data && $.callbackInfo.data.result && $.callbackInfo.data.result.taskToken) {
+    //           await $.wait(8000);
+    //           let sendInfo = encodeURIComponent(`{"dataSource":"newshortAward","method":"getTaskAward","reqParams":"{\\"taskToken\\":\\"${$.callbackInfo.data.result.taskToken}\\"}","sdkVersion":"1.0.0","clientLanguage":"zh"}`)
+    //           await callbackResult(sendInfo)
+    //         } else  {
+    //           await $.wait(2000);
+    //           console.log(`任务完成`);
+    //         }
+    //       }
+    //     }
+    //     await $.wait(1000);
+    //     let boxLotteryNum = $.shopResult.boxLotteryNum;
+    //     for (let j = 0; j < boxLotteryNum; j++) {
+    //       console.log(`开始第${j+1}次拆盒`)
+    //       //抽奖
+    //       await takePostRequest('olympicgames_boxShopLottery');
+    //       await $.wait(3000);
+    //     }
+    //     // let wishLotteryNum = $.shopResult.wishLotteryNum;
+    //     // for (let j = 0; j < wishLotteryNum; j++) {
+    //     //   console.log(`开始第${j+1}次能量抽奖`)
+    //     //   //抽奖
+    //     //   await takePostRequest('zoo_wishShopLottery');
+    //     //   await $.wait(3000);
+    //     // }
+    //     await $.wait(3000);
+    //   }
+    // }
 
 
     // qryCompositeMaterials
