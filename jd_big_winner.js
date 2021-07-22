270a50a54ee0c34bfc4c520fe0a9eda2b6600beb
jsNo1 <79303989+jsNO1@users.noreply.github.com>
2021-07-22 11:51:58 +0800

    Update jd_big_winner.js

--- jd_big_winner.js
+++ jd_big_winner.js
@@ -6,17 +6,17 @@
 =================================Quantumultx=========================
 [task_local]
 #省钱大赢家之翻翻乐
-10 0-23/2 * * * jd_big_winner.js, tag=省钱大赢家之翻翻乐, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+10 0-23/3 * * * jd_big_winner.js, tag=省钱大赢家之翻翻乐, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 
 =================================Loon===================================
 [Script]
-cron "10 0-23/2 * * *" script-path=jd_big_winner.js,tag=省钱大赢家之翻翻乐
+cron "10 0-23/3 * * *" script-path=jd_big_winner.js,tag=省钱大赢家之翻翻乐
 
 ===================================Surge================================
-省钱大赢家之翻翻乐 = type=cron,cronexp="10 0-23/2 * * *",wake-system=1,timeout=3600,script-path=jd_big_winner.js
+省钱大赢家之翻翻乐 = type=cron,cronexp="10 0-23/3 * * *",wake-system=1,timeout=3600,script-path=jd_big_winner.js
 
 ====================================小火箭=============================
-省钱大赢家之翻翻乐 = type=cron,script-path=jd_big_winner.js, cronexpr="10 0-23/2 * * *", timeout=3600, enable=true
+省钱大赢家之翻翻乐 = type=cron,script-path=jd_big_winner.js, cronexpr="10 0-23/3 * * *", timeout=3600, enable=true
  */
 const $ = new Env('省钱大赢家之翻翻乐');
 const notify = $.isNode() ? require('./sendNotify') : '';
@@ -24,7 +24,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message = '', linkId = 'yMVR-_QKRd2Mq27xguJG-w', fflLinkId = 'YhCkrVusBVa_O2K-7xE6hA';
-const money = process.env.BIGWINNER_MONEY || 0.3
+const money = $.isNode() ? (process.env.BIGWINNER_MONEY ? process.env.BIGWINNER_MONEY * 1 : 0.3) : ($.getdata("BIGWINNER_MONEY") ? $.getdata("BIGWINNER_MONEY") * 1 : 0.3)
 const JD_API_HOST = 'https://api.m.jd.com/api';
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
