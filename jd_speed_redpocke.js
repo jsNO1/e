26532cd766d2ca5a88625b743c812c10aaa38a3a
jsNo1 <79303989+jsNO1@users.noreply.github.com>
2021-07-29 15:46:08 +0800

    Update jd_speed_redpocke.js

--- jd_speed_redpocke.js
+++ jd_speed_redpocke.js
@@ -151,7 +151,7 @@ async function sign() {
 function reward_query() {
   return new Promise(resolve => {
     $.get(taskGetUrl("spring_reward_query", {
-      "inviter": ["7eKzr10NLcmTYeaeyUebLw", "r3yIDGE86HSsdtyFlrPHJHu_0mNpX_AnBREYO-c3BFY"][Math.floor((Math.random() * 2))],
+      "inviter": ["7eKzr10NLcmTYeaeyUebLw", "zicY3HSd2gfmfYAc3DZ1OQ","nJ0poKQnWevLvWXw7mM1zA-R24gcXp-ju6zeMFi-r6A"][Math.floor((Math.random() * 3))],
       linkId
     }), async (err, resp, data) => {
       try {
@@ -178,7 +178,7 @@ function reward_query() {
 }
 async function redPacket() {
   return new Promise(resolve => {
-    $.get(taskGetUrl("spring_reward_receive",{"inviter":["7eKzr10NLcmTYeaeyUebLw","r3yIDGE86HSsdtyFlrPHJHu_0mNpX_AnBREYO-c3BFY"][Math.floor((Math.random()*2))],linkId}),
+    $.get(taskGetUrl("spring_reward_receive",{"inviter":["7eKzr10NLcmTYeaeyUebLw","zicY3HSd2gfmfYAc3DZ1OQ","nJ0poKQnWevLvWXw7mM1zA-R24gcXp-ju6zeMFi-r6A"][Math.floor((Math.random()*3))],linkId}),
         async (err, resp, data) => {
           try {
             if (err) {
@@ -438,9 +438,9 @@ function taskGetUrl(function_id, body) {
 function TotalBean() {
   return new Promise(async resolve => {
     const options = {
-      url: "https://me-api.jd.com/user_new/info/GetJDUserInfoUnion",
+      url: "https://wq.jd.com/user_new/info/GetJDUserInfoUnion?sceneval=2",
       headers: {
-        Host: "me-api.jd.com",
+        Host: "wq.jd.com",
         Accept: "*/*",
         Connection: "keep-alive",
         Cookie: cookie,
@@ -457,15 +457,15 @@ function TotalBean() {
         } else {
           if (data) {
             data = JSON.parse(data);
-            if (data['retcode'] === "1001") {
+            if (data['retcode'] === 1001) {
               $.isLogin = false; //cookie过期
               return;
             }
-            if (data['retcode'] === "0" && data.data && data.data.hasOwnProperty("userInfo")) {
+            if (data['retcode'] === 0 && data.data && data.data.hasOwnProperty("userInfo")) {
               $.nickName = data.data.userInfo.baseInfo.nickname;
             }
           } else {
-            $.log('京东服务器返回空数据');
+            console.log('京东服务器返回空数据');
           }
         }
       } catch (e) {
