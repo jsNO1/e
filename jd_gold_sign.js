645b07dda62997b69f62ac350daf42c4574118de
jsNo1 <79303989+jsNO1@users.noreply.github.com>
2022-01-06 17:19:07 +0800

    Update jd_gold_sign.js

--- jd_gold_sign.js
+++ jd_gold_sign.js
@@ -59,6 +59,7 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
         }
         continue
       }
+      await goldCreatorDoTask({ "type": 1 })
       await goldCenterHead();
 
     }
@@ -86,8 +87,8 @@ function goldCenterHead() {
           if (safeGet(data)) {
             data = JSON.parse(data)
             if (data.code === '0') {
-               await goldCreatorDoTask({ "type": 1 })
               if (data.result.medalNum === 5) {
+                await $.wait(1500)
                 await goldCreatorDoTask({ "type": 2 })
               }
             } else {
