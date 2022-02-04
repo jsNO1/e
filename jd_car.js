778164c604daf49f35e126ac26b15e95b1139da3
jsNo1 <79303989+jsNO1@users.noreply.github.com>
2022-02-04 23:03:51 +0800

    Update jd_car.js

--- jd_car.js
+++ jd_car.js
@@ -1,6 +1,7 @@
 /*
 汽车签到
 cron 43 7,21 * * * jd_car.js
+const $ = new Env('汽车签到');
 */
 const name = '汽车签到'
 let UA
@@ -30,7 +31,7 @@ Object.keys(jdCookieNode).forEach((item) => {
         if (cookiesArr[i]) {
             cookie = cookiesArr[i]
             const UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
-            console.log(`\n******开始【京东账号${i}】${UserName}*********\n`);
+            console.log(`\n******开始【京东账号${i + 1}】${UserName}*********\n`);
             UA = process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)
             await main()
         }
